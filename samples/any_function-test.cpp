9be19eb95eb691eeb86ba93d479ee1b9c8f780bd
Sterling Orsten <sgorsten@gmail.com>
2016-05-08 13:52:31 -0700

    Add tests for different arities. Arity-0 currently does not work.

--- samples/any_function-test.cpp
+++ samples/any_function-test.cpp
@@ -40,26 +40,6 @@ TEST_CASE( "any_function is callable with a global function" )
     REQUIRE( *reinterpret_cast<double *>(r.get()) == a*b+c );
 }
 
-TEST_CASE( "any_function can be constructed with a stateless lambda" )
-{
-    const any_function f {[](int a, double b, float c) { return a*b+c; }};
-    REQUIRE( f );
-    REQUIRE( f.get_parameter_types().size() == 3 );
-    REQUIRE( f.get_parameter_types()[0] == &typeid(int) );
-    REQUIRE( f.get_parameter_types()[1] == &typeid(double) );
-    REQUIRE( f.get_parameter_types()[2] == &typeid(float) );
-    REQUIRE( f.get_return_type() == &typeid(double) );
-}
-
-TEST_CASE( "any_function is callable with a stateless lambda" )
-{
-    const any_function f {[](int a, double b, float c) { return a*b+c; }};
-    int a = 5; double b = 12.2; float c = 3.14f;
-    auto r = f.invoke({&a,&b,&c});
-    REQUIRE( r != nullptr );
-    REQUIRE( *reinterpret_cast<double *>(r.get()) == a*b+c );
-}
-
 TEST_CASE( "any_function can be constructed with a std::function" )
 {
     std::function<double(int,double,float)> sf {[](int a, double b, float c) { return a*b+c; }};
@@ -80,4 +60,83 @@ TEST_CASE( "any_function is callable with a std::function" )
     auto r = f.invoke({&a,&b,&c});
     REQUIRE( r != nullptr );
     REQUIRE( *reinterpret_cast<double *>(r.get()) == a*b+c );
+}
+
+/*TEST_CASE( "any_function can be constructed with an arity-0 stateless lambda" )
+{
+    const any_function f {[]() { return 5.0; }};
+    REQUIRE( f );
+    REQUIRE( f.get_parameter_types().size() == 0 );
+    REQUIRE( f.get_return_type() == &typeid(double) );
+}*/
+
+TEST_CASE( "any_function can be constructed with an arity-1 stateless lambda" )
+{
+    const any_function f {[](int a) { return a; }};
+    REQUIRE( f );
+    REQUIRE( f.get_parameter_types().size() == 1 );
+    REQUIRE( f.get_parameter_types()[0] == &typeid(int) );
+    REQUIRE( f.get_return_type() == &typeid(int) );
+}
+
+TEST_CASE( "any_function can be constructed with an arity-2 stateless lambda" )
+{
+    const any_function f {[](int a, double b) { return a*b; }};
+    REQUIRE( f );
+    REQUIRE( f.get_parameter_types().size() == 2 );
+    REQUIRE( f.get_parameter_types()[0] == &typeid(int) );
+    REQUIRE( f.get_parameter_types()[1] == &typeid(double) );
+    REQUIRE( f.get_return_type() == &typeid(double) );
+}
+
+TEST_CASE( "any_function can be constructed with an arity-3 stateless lambda" )
+{
+    const any_function f {[](int a, double b, float c) { return a*b+c; }};
+    REQUIRE( f );
+    REQUIRE( f.get_parameter_types().size() == 3 );
+    REQUIRE( f.get_parameter_types()[0] == &typeid(int) );
+    REQUIRE( f.get_parameter_types()[1] == &typeid(double) );
+    REQUIRE( f.get_parameter_types()[2] == &typeid(float) );
+    REQUIRE( f.get_return_type() == &typeid(double) );
+}
+
+TEST_CASE( "any_function can be constructed with an arity-4 stateless lambda" )
+{
+    const any_function f {[](int a, double b, float c, short d) { return a*b+c*d; }};
+    REQUIRE( f );
+    REQUIRE( f.get_parameter_types().size() == 4 );
+    REQUIRE( f.get_parameter_types()[0] == &typeid(int) );
+    REQUIRE( f.get_parameter_types()[1] == &typeid(double) );
+    REQUIRE( f.get_parameter_types()[2] == &typeid(float) );
+    REQUIRE( f.get_parameter_types()[3] == &typeid(short) );
+    REQUIRE( f.get_return_type() == &typeid(double) );
+}
+
+TEST_CASE( "any_function is callable with an arity-3 stateless lambda" )
+{
+    const any_function f {[](int a, double b, float c) { return a*b+c; }};
+    int a = 5; double b = 12.2; float c = 3.14f;
+    auto r = f.invoke({&a,&b,&c});
+    REQUIRE( r != nullptr );
+    REQUIRE( *reinterpret_cast<double *>(r.get()) == a*b+c );
+}
+
+double g;
+TEST_CASE( "any_function can be constructed with an arity-3 stateless lambda returning void" )
+{
+    const any_function f {[](int a, double b, float c) { g=a*b+c; }};
+    REQUIRE( f );
+    REQUIRE( f.get_parameter_types().size() == 3 );
+    REQUIRE( f.get_parameter_types()[0] == &typeid(int) );
+    REQUIRE( f.get_parameter_types()[1] == &typeid(double) );
+    REQUIRE( f.get_parameter_types()[2] == &typeid(float) );
+    REQUIRE( f.get_return_type() == &typeid(void) );
+}
+
+TEST_CASE( "any_function is callable with an arity-3 stateless lambda returning void" )
+{
+    const any_function f {[](int a, double b, float c) { g=a*b+c; }};
+    int a = 5; double b = 12.2; float c = 3.14f;
+    auto r = f.invoke({&a,&b,&c});
+    REQUIRE( r == nullptr );
 }
